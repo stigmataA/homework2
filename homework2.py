55922df35ca240466b9f01fb59d42c15a8a598fd
stigmataA <r.muhutdinov@gmail.com>
2024-07-29 17:49:07 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_homework = 12
+Number_of_hours_spent = 1.5
+Name_of_cource = "Python"
+Time_per_task = (Number_of_hours_spent / number_of_homework)
+print('Курс:', Name_of_cource,",", 'всего задач:', number_of_homework,"," 'затрачено часов:', Number_of_hours_spent, ","'среднее время выполнения:', Time_per_task)
